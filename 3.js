dbb8a136582506bb6b4f1e076bb689e23bfb6522
Sofiya12456 <udovinas@gmail.com>
2025-03-27 15:42:53 +0500

    Add files via upload

---
+++ 3.js
@@ -0,0 +1,15 @@
+const navLinks = document.querySelectorAll('.nav-link');
+const sections = document.querySelectorAll('.section');
+
+
+navLinks.forEach(link => {
+    link.addEventListener('click', event => {
+        event.preventDefault(); 
+        const targetId = link.getAttribute('href').substring(1);
+
+       
+        sections.forEach(section => {
+            section.classList.toggle('active', section.id === targetId);
+        });
+    });
+});
