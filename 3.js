e6751f12128c7346712066ce5fbf9d172ff34557
sofiya12456 <udovinas@gmail.com>
2025-04-03 15:32:12 +0500

    3.js

--- 3.js
+++ 3.js
@@ -1,13 +1,11 @@
 const navLinks = document.querySelectorAll('.nav-link');
 const sections = document.querySelectorAll('.section');
 
-
 navLinks.forEach(link => {
     link.addEventListener('click', event => {
-        event.preventDefault(); 
+        event.preventDefault();
         const targetId = link.getAttribute('href').substring(1);
 
-       
         sections.forEach(section => {
             section.classList.toggle('active', section.id === targetId);
         });
