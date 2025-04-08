bab47b5d3ff0ca5918f3fe39b1d6f606b9543b93
sofiya12456 <udovinas@gmail.com>
2025-04-08 20:45:38 +0600

    3.js

--- 3.js
+++ 3.js
@@ -1,13 +1,18 @@
 const navLinks = document.querySelectorAll('.nav-link');
+
 const sections = document.querySelectorAll('.section');
 
 navLinks.forEach(link => {
-    link.addEventListener('click', event => {
-        event.preventDefault();
+    link.addEventListener('click', () => {
+    
         const targetId = link.getAttribute('href').substring(1);
 
         sections.forEach(section => {
-            section.classList.toggle('active', section.id === targetId);
+            if (section.id === targetId) {
+                section.classList.add('active');
+            } else {
+                section.classList.remove('active');
+            }
         });
     });
 });
